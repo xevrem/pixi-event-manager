c73ee7c1358697d544bd46480617eec60478b0d0
erika jonell <erika.jonell@gmail.com>
2019-10-05 09:04:01 -0400

    basic event tracker

--- src/index.js
+++ src/index.js
@@ -19,37 +19,81 @@ window.addEventListener('load', ()=>{
     backgroundColor: 0x1099bb,
     resolution: window.devicePixelRatio || 1,
   });
-  IM = app.renderer.plugins.interaction;
-  //console.dir('im:', IM);
 
-  IM.on('pointerdown', globalWatch);
-  IM.on('pointerup', globalWatch);
-  //IM.on('pointermove', globalWatch);
+  InteractionTracker.init(app.renderer.plugins.interaction);
 
   main.append(app.view);
 
 
   const circle = new Pixi.Graphics();
   circle.interactive = true;
-  circle.__id = 'foobar';
+  circle.__id = 'foo';
   circle
     .beginFill(0xff5555)
-    .drawCircle(300,300,100)
+    .drawCircle(200,200,100)
     .endFill();
 
-  //circle.on('mouseover', handleMouseOver);
+
+  const rect = new Pixi.Graphics();
+  rect.interactive = true;
+  rect.__id = 'bar';
+  rect
+    .beginFill(0xffff55)
+    .drawRect(400,400, 100, 100)
+    .endFill();
+
 
 
   app.stage.addChild(circle);
+  app.stage.addChild(rect);
 });
 
-const handleMouseOver = (event) => {
-  console.log('hmo', event);
-};
+
+const InteractionData = (id) => ({
+  id
+});
+
+class InteractionTracker {
+  static init(interactionManager, stage){
+    //hook into the IM
+    this.__IM = interactionManager;
+    this.__IM.on('pointerdown', this.globalWatch, {context:this, event:'pointerdown'});
+    this.__IM.on('pointerup', this.globalWatch, {context:this, event:'pointerup'});
+    this.__IM.on('pointermove', this.globalWatch, {context:this, event:'pointermove'});
+    this.__IM.on('pointerover', this.globalWatch, {context:this, event:'pointerover'});
+    this.__IM.on('pointerout', this.globalWatch, {context:this, event:'pointerout'});
+
+    this.__stage = stage;
+    this.__interactions = {
+      stage: InteractionData('stage')
+    };
+    this.__lastInteractive = null;
+  }
+
+  static globalWatch(event){
+    // test to see if there is anything here
+    const result = this.context.__IM.hitTest(event.data.global, this.context.__stage);
+    this.context.update(event, result, this.event);
+    console.log('gw', this.context.__interactions);
+  };
 
 
-const globalWatch = event => {
-  const result = IM.hitTest(event.data.global, app.stage);
-  console.log('result:', result);
-  console.log('event type:', event.type);
+  static update(event, entity, baseEvent){
+    if(entity){
+      // this is a DisplayObject
+      if(!this.__interactions.hasOwnProperty(entity.__id)){
+        this.__interactions[entity.__id] = InteractionData(entity.__id);
+      }
+      // flag the event as having occurred
+      this.__interactions[entity.__id][baseEvent] = event.type;
+    }else{
+      // this is the stage
+      this.__interactions.stage[baseEvent] = event.type;
+    }
+    this.__lastInteractive = entity;
+  }
+
+  static getInteractions(){
+    return this.__interactions;
+  }
 }
